87ca585199237934a85c306cacdf1513930a542b
Hudson <0001155678@senaimgaluno.com.br>
2025-11-03 10:21:55 -0300

    Alteração na Mensagem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bem vindo à pratica de clone do Wesley e do Hudson!");
+        System.out.println("Tudo ok");
     }
 }
